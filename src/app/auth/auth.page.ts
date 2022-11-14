dd35b00ce90c0806bfbcb1a0ab072509e127c7c1
Piotr Machnicki <piotrmachnicki1989@gmail.com>
2022-11-14 21:45:55 +0100

    forms

--- src/app/auth/auth.page.ts
+++ src/app/auth/auth.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 import { Router } from '@angular/router';
 import { LoadingController } from '@ionic/angular';
 import { AuthService } from './auth.service';
@@ -10,6 +11,8 @@ import { AuthService } from './auth.service';
 })
 export class AuthPage implements OnInit {
   isLoading = false;
+  isLogin = true;
+
   constructor(
     private authService: AuthService,
     private router: Router,
@@ -34,5 +37,25 @@ export class AuthPage implements OnInit {
           this.router.navigateByUrl('/places/tabs/discover');
         }, 1500);
       });
+  }
+
+  onSubmit(form: NgForm) {
+    if (!form.valid) {
+      return;
+    }
+
+    const email = form.value.email;
+    const pass = form.value.password;
+    console.log(email, pass);
+
+    if (this.isLogin) {
+      // login request
+    } else {
+      // signup request
+    }
+  }
+
+  onSwitchAuthMode() {
+    this.isLogin = !this.isLogin;
   }
 }
